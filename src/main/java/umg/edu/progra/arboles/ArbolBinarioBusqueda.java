ecd86011901c6d4d512d42e756ac7dbca53d41b0
AlejandroGCampos <egodoyc4@miumg.edu.gt>
2026-06-03 20:51:50 -0600

    feat: solucion a problema 4 ancestroComunMasBajo

--- src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
+++ src/main/java/umg/edu/progra/arboles/ArbolBinarioBusqueda.java
@@ -262,7 +262,33 @@ public class ArbolBinarioBusqueda {
                 && esBSTValidoRecursivo(nodo.derecho, nodo.dato, maximo);
     }
     
-    //
+    //Problema 4 (Solución)
+    public int ancestroComunMasBajo(int a, int b) {
+
+        if (!contiene(a) || !contiene(b)) {
+            throw new IllegalArgumentException(
+                    "Ambos valores deben existir en el arbol");
+        }
+
+        return ancestroComunMasBajoRecursivo(raiz, a, b);
+    }
+
+    private int ancestroComunMasBajoRecursivo(Nodo nodo, int a, int b) {
+
+        if (a < nodo.dato && b < nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.izquierdo, a, b);
+        }
+
+        if (a > nodo.dato && b > nodo.dato) {
+            return ancestroComunMasBajoRecursivo(
+                    nodo.derecho, a, b);
+        }
+
+        return nodo.dato;
+    }
+    
+    //Problema 5 (Solución)
     
 
     // ============================================================
